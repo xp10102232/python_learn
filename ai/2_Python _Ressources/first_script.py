25a481b64a1bb77300c20c8986d55f1af23cb61d
Pan Xi <PXi@slb.com>
2023-03-24 09:35:46 +0800

    init

---
+++ ai/2_Python _Ressources/first_script.py
@@ -0,0 +1,17 @@
+# Some imports
+import os
+import numpy
+
+SOME_CONSTANT = 10
+
+
+def some_function(some_input):
+    some_output = None
+    return some_output
+
+
+some_variables = 1
+some_variables += 2
+
+# Some comments
+print("Some prints.")
